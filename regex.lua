225763dceeb4f3b753ec4d52379fb69d10cf0e6f
Martin Zeller <mz.bremerhaven@gmail.com>
2021-06-17 01:38:43 +0200

    Fix regex stubs

--- regex.lua
+++ regex.lua
@@ -1,20 +1,27 @@
 ---@class regex
 regex = {}
 
+---@class Regex
+local Regex = {}
+
 ---@param pattern string
+---@return Regex
 function regex.new(pattern)
 end
 
 ---@param str string
-function regex:test(str)
+---@return boolean
+function Regex:test(str)
 end
 
 ---@param str string
-function regex:match(str)
+---@return string[]
+function Regex:match(str)
 end
 
 ---@param str string
 ---@param replace string
 ---@param count number?
-function regex:replace(str, replace, count)
+---@return string
+function Regex:replace(str, replace, count)
 end
